eb130a48eac1d5fd50cbe6cc04f69e65109a3b26
Aditya Sharat <adityasharat@fb.com>
2020-12-21 18:25:07 +0000

    add API methods

--- index.js
+++ index.js
@@ -9,16 +9,56 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(parser.json())
 
-let films = [];
-
-fs.readFile('./films.json', 'utf-8', (err, data) => {
- films = JSON.parse(data)
+app.get('/', (req, res) => {
+  read((error, films) => {
+    if (!error) {
+      res.send(films)
+    } else {
+      res.status(500).send({
+        error: error.message
+      })
+    }
+  })
 })
 
-app.get('/', (req, res) => {
-  res.send(films)
+app.post('/', (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2))
+  read((error, films) => {
+    if (!error) {
+      films.push(req.body)
+        write(films, (error) => {
+          if (!error) {
+          res.send(films)
+        } else {
+          res.status(500).send({
+            error: error.message
+          })
+        }
+     })
+    } else {
+      res.status(500).send({
+        error: error.message
+      })
+    }
+  })
+})
+
+app.delete('/:index', (req, res) => {
+
 })
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
+
+function read(done) {
+  fs.readFile('./films.json', (error, data) => {
+    done(error, JSON.parse(data))
+  })
+}
+
+function write(films, done) {
+  fs.writeFile('./films.json', JSON.stringify(films, null, 2), (error) => {
+    done(error)
+  })
+}
